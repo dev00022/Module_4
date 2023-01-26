cd470f3af2242adf5b50c8efb937e9ebc805d84b
dev00022 <118600186+dev00022@users.noreply.github.com>
2023-01-27 01:57:46 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,10 +1,20 @@
+let speakWord = "Good Bye";
+function byespeak(name){
+    console.log(speakWord + " " + name);
+}
+
+let speakWord2 = "Hello";
+function hellospeak(name){
+    console.log(speakWord2 + " " + name);
+}
+
 let names = ["Yaakov","John","Jen","Jason","Paul","Frank","Larry","Paula","Laura","Jim"];
 for(let i=0; i<names.length; i++){
     let first = names[i].charAt(0).toLocaleLowerCase();
     if(first === "j"){
-        console.log("Good Buy"+" "+names[i]);
+        byespeak(names[i]);
     }
     else{
-        console.log("Hello"+ " "+names[i]);
+        hellospeak(names[i]);
     }
 }  
